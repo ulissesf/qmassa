eabff3fad0c5077433b11e767d031a904943f098
Ulisses Furquim <ulissesf@gmail.com>
2024-09-08 18:42:34 -0700

    qmdevice: store card and render dev nodes' (major, minor)

--- src/qmdevice.rs
+++ src/qmdevice.rs
@@ -1,18 +1,34 @@
 use anyhow::Result;
+use libc;
 use udev;
 
 
 #[derive(Debug)]
 pub struct QmDevice
 {
-    drm_card: String,
-    drm_render: String,
     subsystem: String,
+    drm_card: String,
+    drm_card_devnum: (u32, u32),
+    drm_render: String,
+    drm_render_devnum: (u32, u32),
     sysname: String,            // same as PCI_SLOT_NAME
     syspath: String,
     vendor_id: String,
     device_id: String,
     driver: String,
+}
+
+fn mj_mn_from_devnum(dnum: u64) -> (u32, u32)
+{
+    let mj: u32;
+    let mn: u32;
+
+    unsafe {
+        mj = libc::major(dnum);
+        mn = libc::minor(dnum);
+    }
+
+    (mj, mn)
 }
 
 impl QmDevice
@@ -31,9 +47,11 @@ impl QmDevice
              let pciid = pdev.property_value("PCI_ID").unwrap().to_str().unwrap();
 
              let qmd = QmDevice {
-                drm_card: String::from(d.devnode().unwrap().to_str().unwrap()),
-                drm_render: String::from(""),
                 subsystem: String::from(d.subsystem().unwrap().to_str().unwrap()),
+                drm_card: String::from(d.devnode().unwrap().to_str().unwrap()),
+                drm_card_devnum: mj_mn_from_devnum(d.devnum().unwrap()),
+                drm_render: String::from(""),
+                drm_render_devnum: (0, 0),
                 sysname: String::from(pdev.sysname().to_str().unwrap()),
                 syspath: String::from(pdev.syspath().to_str().unwrap()),
                 vendor_id: String::from(&pciid[0..4]),
@@ -52,6 +70,7 @@ impl QmDevice
             for rd in &rend {
                 if rd.syspath == cd.syspath {
                     cd.drm_render = rd.drm_card.clone();
+                    cd.drm_render_devnum = rd.drm_card_devnum;
                     break;
                 }
             }
