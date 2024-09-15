35ce2a901c20478a3ca696f9814886de2fe89614
Ulisses Furquim <ulissesf@gmail.com>
2024-09-15 01:26:57 -0700

    Add couple more TODOs.

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ fn main() -> Result<()>
     let args = Args::parse();
     let base_pid = args.pid.unwrap();
 
+    // TODO: if base_pid == 1 && not root, set base_pid to first pid from user
+
     let qmds = QmDevice::find_devices().context("Failed to find DRM devices")?;
     for d in &qmds {
         println!("{:#?}", d);
@@ -35,6 +37,8 @@ fn main() -> Result<()>
     let pst = QmProcInfo::from_pid_tree(&base_pid, &qmds)
         .with_context(|| format!("Failed to get proc data from tree at {:?}", base_pid))?;
     println!("{:#?}", pst);
+
+    // TODO: add text-based UI
 
     Ok(())
 }
