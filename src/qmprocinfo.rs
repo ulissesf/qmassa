1ec25c405894061ddd487abe885676512ebd1c7e
Ulisses Furquim <ulissesf@gmail.com>
2024-09-22 23:46:37 -0700

    Handle fd and fdinfo entries volatility in /proc.

--- src/qmprocinfo.rs
+++ src/qmprocinfo.rs
@@ -3,6 +3,7 @@ use std::path::{Path, PathBuf};
 use std::fs;
 
 use anyhow::Result;
+use log::debug;
 
 use crate::qmdrmfdinfo::QmDrmFdinfo;
 
@@ -47,12 +48,23 @@ impl QmProcInfo
             let et = et?;
 
             let mut mn: u32 = 0;
-            if !QmDrmFdinfo::is_drm_fd(&et.path(), &mut mn)? {
+            let is_drm_fd = QmDrmFdinfo::is_drm_fd(&et.path(), &mut mn);
+            if let Err(err) = is_drm_fd {
+                debug!("ERR: failed to find fd {:?}: {:?}", et.path(), err);
+                continue;
+            }
+            let is_drm_fd = is_drm_fd.unwrap();
+            if !is_drm_fd {
                 continue;
             }
 
             let fipath = fdinfodir.join(et.path().file_name().unwrap());
-            let finfo = QmDrmFdinfo::from(&fipath, mn)?;
+            let finfo = QmDrmFdinfo::from(&fipath, mn);
+            if let Err(err) = finfo {
+                debug!("ERR: failed to parse DRM fdinfo {:?}: {:?}", fipath, err);
+                continue;
+            }
+            let finfo = finfo.unwrap();
 
             res.push(finfo);
         }
